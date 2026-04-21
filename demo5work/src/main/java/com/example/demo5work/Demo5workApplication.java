806407dfd479908dff6ba0e202ac4a7274a96b64
Liangxian Lv <1253639565@qq.com>
2026-04-21 21:20:42 +0800

    demo5work

---
+++ demo5work/src/main/java/com/example/demo5work/Demo5workApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo5work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo5workApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo5workApplication.class, args);
+    }
+
+}
